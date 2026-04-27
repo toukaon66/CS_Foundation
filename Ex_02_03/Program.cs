dc2c18c8d8efac95987310db74ae45c12d5a3b80
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-04-27 17:05:33 +0900

    演習３

---
+++ Ex_02_03/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex_02_03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("数値を入力してください->");
+var number = System.Console.ReadLine();
+int result;
+result = int.Parse(number) + 5;
+System.Console.WriteLine($"5を加算した結果は{result}です");
+result = int.Parse(number) - 5;
+System.Console.WriteLine($"5を減算した結果は{result}です");
+    }
+}
