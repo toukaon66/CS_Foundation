6905b6aa4cc4e27d9a6a318666dce95e59bf00c5
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-04-27 16:20:48 +0900

    hozonn

--- Example/Program.cs
+++ Example/Program.cs
@@ -26,9 +26,9 @@ static class Program
         // a = a + 100;
         // System.Console.WriteLine(a);
 
-        int a = 5;
-        int b = 2;
-        float c = (float)a / b;
+        // int a = 5;
+        // int b = 2;
+        // float c = (float)a / b;
         //一時的に型変換　より精度が高い（細かい数字）ほうが優先される　同じ演算型だと同じ演算型の結果しか出ない
         //今回の場合（float）aがないときはint（整数）だから小数点以下が出なくなる
         // System.Console.WriteLine(c);*/
@@ -38,5 +38,19 @@ static class Program
         TAX_RATE=10.0;//行を変えての宣言はできない*/
 
         //System.Console.Write(c);
+
+        ﻿// 数値の演算
+Console.WriteLine($"10 + 3 = {10 + 3}");
+Console.WriteLine($"10 - 3 = {10 - 3}");
+Console.WriteLine($"10 * 3 = {10 * 3}");
+Console.WriteLine($"10 / 3 = {10 / 3}");
+Console.WriteLine($"10 % 3 = {10 % 3}");
+// 文字列の連結
+Console.WriteLine($"ABC + XYZ = {"ABC" + "XYZ"}");
+// 文字列と他のデータ型の結合
+Console.WriteLine($"ABC + 1 = {"ABC" + 1}");
+// その他、文字列の結合と表示（参考）
+Console.WriteLine($"ABC + 2 = {"ABC"}{2}");
+Console.WriteLine("ABC + 3 = {0}{1}", "ABC", 3);
     }
 }
