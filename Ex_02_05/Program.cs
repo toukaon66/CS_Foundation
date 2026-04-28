5e67fe351a51956ecf8bd260a2266ef9a35d097d
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-04-28 11:16:30 +0900

    演習５

---
+++ Ex_02_05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Ex_02_05;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+      
+       Console.Write("数値を入力してください->");
+       int number = int.Parse(System.Console.ReadLine());
+        if(number % 2 == 0)
+        {
+            Console.WriteLine($"結果:{number}");
+        }
+        else
+        {
+             {Console.WriteLine($"結果:{number*5}");}
+        }  
+    }     
+}
