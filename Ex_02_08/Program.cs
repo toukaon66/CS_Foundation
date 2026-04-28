2b5f1a914f81509a7da9e754141410110fa6a657
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-04-28 12:52:44 +0900

    演習８

--- Ex_02_08/Program.cs
+++ Ex_02_08/Program.cs
@@ -4,6 +4,13 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] station = { "品川", "大崎", "五反田", "目黒", "恵比寿", "渋谷", "原宿", "代々木", "新宿" };
+        var count=0;
+    while(count < 9 )
+        {
+            Console.WriteLine($"{station[count]}");
+                        count=count+1;
+
+        }
     }
 }
