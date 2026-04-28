5b51487d472cd5dacf0a1b47f6019635e2b67d41
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-04-28 10:54:32 +0900

    演習４

---
+++ Ex_02_04/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Ex_02_04;
+
+static class Program
+{
+    static void Main(String[] args)
+    {
+        int[] number=  new int[3];
+       
+        Console.Write("数値1を入力してください->");
+        number[0] = int.Parse(System.Console.ReadLine());
+        Console.Write("数値2を入力してください->");
+        number[1] = int.Parse(System.Console.ReadLine());
+        Console.Write("数値3を入力してください->");
+        number[2] = int.Parse(System.Console.ReadLine());
+        Console.WriteLine("入力結果");
+        Console.WriteLine($"数値1:{number[0]}");
+        Console.WriteLine($"数値2:{number[1]}");
+        Console.WriteLine($"数値3:{number[2]}");
+
+
+        // int[] number = new int[3];
+
+        // Console.Write("数値1を入力してください->");
+        // number[0] = int.Parse(Console.ReadLine());
+        // Console.Write("数値2を入力してください->");
+        // number[1] = int.Parse(Console.ReadLine());
+        // Console.Write("数値3を入力してください->");
+        // number[2] = int.Parse(Console.ReadLine());
+
+        // Console.WriteLine("入力結果");
+        // Console.WriteLine($"数値1:{number[0]}");
+        // Console.WriteLine($"数値2:{number[1]}");
+        // Console.WriteLine($"数値3:{number[2]}");
+    }
+}
