015f0d7c44b9da88c4b9324979346f1b3e929018
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-04-28 11:23:00 +0900

    演習６

---
+++ Ex_02_06/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex_02_06;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        int number = int.Parse(System.Console.ReadLine());
+         if(number % 4 == 0)
+           Console.WriteLine("4の倍数です");
+           else
+        {
+             Console.WriteLine("4の倍数ではありません");
+        }
+    }
+}
