5aa3b9466ad1819d43755c326324f0266d78bbd5
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-04-30 17:05:57 +0900

    3-9

---
+++ Ex_03_09/Modules/Account.cs
@@ -0,0 +1,25 @@
+namespace Ex_03_09;
+
+public class Account
+{
+public string? AccountNo { get; set; }	//口座番号を表すプロパティ
+public string? AccountName { get; set; }	//口座名義を表すプロパティ
+public int Balance { get; set; }	//残高を表すプロパティ
+
+
+
+public Account(string AccountNo, string AccountName, int Balance)
+    {
+        
+    }
+    
+
+
+// public void Print()
+//     {
+//         System.Console.WriteLine($"口座番号＝{AccountNo}");
+//         System.Console.WriteLine($"口座名義{AccountName}");
+//         System.Console.WriteLine($"残高＝{Balance}");
+//     }
+
+}
---
+++ Ex_03_09/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex_03_09;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+
+    //      Netaccount netaccount=new();
+    //     System.Console.Write("口座番号を入力してください->");
+    //     netaccount.AccountNo = System.Console.ReadLine();
+    //     System.Console.Write("口座名義を入力してください->");
+    //     netaccount.AccountName = System.Console.ReadLine();
+    //     System.Console.Write("残高を入力してください->");
+    //     netaccount.Balance = int.Parse(System.Console.ReadLine());
+    //     System.Console.Write("ポイントを入力してください->");
+    //     netaccount.Point = int.Parse(System.Console.ReadLine());
+    //     System.Console.Write("結果");
+
+    //    netaccount.Print();
+    }
+}
